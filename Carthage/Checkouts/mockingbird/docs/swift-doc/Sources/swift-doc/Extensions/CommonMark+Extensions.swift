6b0379d3da4e05aa10caa6e900c64c34885725ee
Jacob Rakidzich <jacob.rakidzich@daveramsey.com>
2020-08-09 19:49:27 -0500

    rm carthage dir

--- Carthage/Checkouts/mockingbird/docs/swift-doc/Sources/swift-doc/Extensions/CommonMark+Extensions.swift
+++
@@ -1,8 +0,0 @@
-import CommonMark
-import HypertextLiteral
-
-extension CommonMark.Document: HypertextLiteralConvertible {
-    public var html: HypertextLiteral.HTML {
-        HypertextLiteral.HTML(render(format: .html, options: .unsafe))
-    }
-}
--- Carthage/Checkouts/mockingbird/docs/swift-doc/Sources/swift-doc/Extensions/HypertextLiteral+Extensions.swift
+++
@@ -1,9 +0,0 @@
-import HypertextLiteral
-import CommonMark
-
-extension HypertextLiteral.HTML.StringInterpolation {
-    mutating func appendInterpolation(commonmark: String) {
-        guard let document = try? Document(commonmark) else { return }
-        appendLiteral(document.render(format: .html, options: [.unsafe]))
-    }
-}
